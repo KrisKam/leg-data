2ced414da2c2337ca49d92363c0fe20f3a24ab66
Kris Kamikawa <kris.kamikawa@gmail.com>
2018-10-21 22:45:06 -0600

    Added recursion to bills and created bills.json file with bill summary info

--- bills.js
+++ bills.js
@@ -1,49 +1,57 @@
-
 const puppeteer = require("puppeteer");
 
 (async () => {
 
-  // Bill info on the page, recursively check the next page in the URL pattern
-  const extractBillInfo = async url => {
-    
-    // Scrape the data we want
+  //Extract bills on page, recursively check next page in URL pattern.
+  const extractBills = async url => {
+
+    //Scrape the data
     const page = await browser.newPage();
     await page.goto(url);
-    const billInfoOnPage = await page.evaluate(() => {
 
-      const bill = {};
-
-      Array.from(document.querySelectorAll("div.views-row > article > div > header > div > div > div")).forEach((section) => {
-
-        console.log("is something here?")
-        bill["number"] = section.innerText
-      })
-      return bill}
-    );
+    const billsOnPage = await page.evaluate(() => {
+    
+      return Array.from(document.querySelectorAll("div.view-content > div.views-row > article")).map((section, i) => {
+        const bill = {};
   
- 
+        bill["bill"] = section.querySelector("div.search-main > header > div > div > div.field-item").innerText;
+        bill["title"] = section.querySelector("div.search-main > header > h4 > a").innerText;
+        bill["bill_url"] = section.querySelector("div.search-main > header > h4 > a").href;
+        bill["description"] = section.querySelector("div.search-main > header > div.field-name-field-bill-long-title > div.field-items > div.field-item").innerText;
+        bill["last_action"] = section.querySelector("div.search-main > div.bill-last-action > span").innerText;
+        
+        let sponsorArray = Array.from(section.querySelectorAll("div.search-main > div.bill-sponsors a")).map(sponsor => sponsor.innerText)
+        bill["sponsors"] = sponsorArray
+  
+        let subjectArray = Array.from(section.querySelectorAll("div.search-aside > div.bill-subjects div.field-item")).map(subject => subject.innerText);
+        bill["subject"] = subjectArray;
+        
+        return bill
+      })
+    })
+
     await page.close();
 
-    // Recursively scrape the next page
-    if (billInfoOnPage.length < 1) {
-      // Terminate if no bills exist
-      return billInfoOnPage
+    //Recursively scrape the next page
+    if (billsOnPage.length < 1) {
+      //Terminate if no bills exist
+      return billsOnPage;
     } else {
-      // Go fetch the next page ?page=X+1
-      const nextPageNumber = parseInt(url.match(/page=(\d+)$/)[1], 10) + 1;
-      const nextUrl = `https://leg.colorado.gov/bill-search?field_chamber=All&field_bill_type=75&field_sessions=45771&sort_bef_combine=field_bill_number%20ASC&page=${nextPageNumber}`;
+      //Go fetch the next page
+      const nextPageNumber = parseInt(url.match(/page=(\d+)$/)[1], 10) +1;
+      const nextUrl = `https://leg.colorado.gov/bill-search?field_chamber=All&field_bill_type=75&field_sessions=45771&sort_bef_combine=field_bill_number%20ASC&page=${nextPageNumber}`
 
-      return billInfoOnPage.concat(await extractBillInfo(nextUrl))
+      return billsOnPage.concat(await extractBills(nextUrl))
     }
+
   };
 
+
   const browser = await puppeteer.launch();
-  const firstUrl =
-    "https://leg.colorado.gov/bill-search?field_chamber=All&field_bill_type=75&field_sessions=45771&sort_bef_combine=field_bill_number%20ASC&page=24";
-  const bills = await extractBillInfo(firstUrl);
+  const firstUrl = "https://leg.colorado.gov/bill-search?field_chamber=All&field_bill_type=75&field_sessions=45771&sort_bef_combine=field_bill_number%20ASC&page=0";
+  const bills = await extractBills(firstUrl);
 
-  // Todo: Update database with bills
-  // console.log(JSON.stringify(bills));
-
+  console.log(JSON.stringify(bills))
+ 
   await browser.close();
 })();
