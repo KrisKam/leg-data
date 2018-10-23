35cbaad94aa42a731545f025026cbf08fe5a88f3
Kris Kamikawa <kris.kamikawa@gmail.com>
2018-10-23 12:44:20 -0600

    Find url for vote results page

--- voteSecond.js
+++ voteSecond.js
@@ -1,21 +1,50 @@
-const puppeteer = require('puppeteer');
+
+//Tuesday
+
+const puppeteer = require("puppeteer");
 
 (async () => {
   try {
-    const browser = await puppeteer.launch({headless: false});
-    const page = await browser.newPage();
-    await page.goto("view-source:https://leg.colorado.gov/bills/sb18-002");
+  const browser = await puppeteer.launch({headless: true});
+  const page = await browser.newPage();
 
-    const voteTab = document.querySelector("div#bill-documents-tabs4 > h2")
+  await page.goto("https://leg.colorado.gov/bills/sb18-002");
 
-    console.log(voteTab.innerText)
+  const chamberSelection = await page.evaluate(() => {  
+    const items = {};
+    const rows = document.querySelectorAll("#bill-documents-tabs4 tr");
+    return Array.from(rows).map((row, index) => {
+      // const items = {};
+      Array.from(row.querySelectorAll("td div.field-item")).forEach((item) => {
+        if (item.innerText === "REPASS") {
+          let votePageLink = row.querySelector("a").href
+          items["repass"] = votePageLink
+          
+        } else if (item.innerText === "BILL") {
+          let votePageLink = row.querySelector("a").href
+          items["bill"] = votePageLink
+        } 
+     })
+      // return items
+    })
+    return items
+  })
 
-    // await page.screenshot({path: 'example.png'});
+  console.log("selection: ", chamberSelection)
+ 
+  // const finalVotes = await page.evaluate(() => {
+  //   return Array.from(document.querySelectorAll("tbody tr")).map(row => {
+      
+  //     return person
+  //   })
+  // })
+  // console.log(JSON.stringify(finalVotes))
+  await page.screenshot({path: 'example.png'});
 
-    await browser.close();
-
-}catch(error){
-  console.error("error: ", error);
-}
+  await browser.close();
+  }catch(error){
+    console.error("error: ", error);
+  }
 
 })();
+
--- votes.js
+++ votes.js
@@ -2,30 +2,32 @@ const puppeteer = require("puppeteer");
 
 (async () => {
   try {
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch({headless: true});
   const page = await browser.newPage();
 
-  await page.goto("https://leg.colorado.gov/bills/sb18-001");
+  await page.goto("https://leg.colorado.gov/bills/sb18-002");
 
   const chamberSelection = await page.evaluate(() => {
-    const rows = document.querySelectorAll("#bill-documents-tabs4 tr");
-    return Array.from(rows).map((row, index) => {
-      const items = [];
-      Array.from(row.querySelectorAll("td div.field-item")).forEach((item, i) => {
-        if (item.innerText === "REPASS")
-        items.push(row.querySelector("a").href)
-        let votePageLink = row.querySelector("a").href
-        page.click(votePageLink)
-      
-      })
-      return items
+    const chamberVoteSections = document.querySelectorAll("#bill-documents-tabs4 > ul > li");
+    return Array.from(chamberVoteSections).map((voteSection) => {
+      return Array.from(voteSection.querySelectorAll("td[data-label='Motion'] div.field-item")).map((item) => {
+        const items = {};
+        if (item.innerText === "REPASS") {
+          let votePageLink = voteSection.querySelector("tbody a").href
+          items["repass"] = votePageLink
+        } else if (item.innerText === "BILL") {
+          let votePageLink = voteSection.querySelector("tbody a").href
+          items["bill"] = votePageLink
+        } 
+        return items
+      }) 
     })
   })
 
   console.log("cs: ", chamberSelection)
  
   // const finalVotes = await page.evaluate(() => {
-  //   return Array.from(document.querySelectorAll("tbody tr")).map(row => {
+  //   return Array.from(document.querySelectorAll("tbody tr")).map(voteSection => {
       
   //     return person
   //   })
